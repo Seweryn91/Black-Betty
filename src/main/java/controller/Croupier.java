53ca6ac169da43a87148d379738f6777ec694580
Seweryn91 <s.talaj@gmail.com>
2020-01-09 21:30:14 +0100

    Create class Croupier, implement constructor and method dealCards

---
+++ src/main/java/controller/Croupier.java
@@ -0,0 +1,38 @@
+package controller;
+
+import model.Card;
+import model.Deck;
+import model.Player;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Croupier {
+
+    private Deck deck;
+    private List<Player> players;
+
+    public Croupier(Deck deck, List<Player> players) {
+        this.deck = deck; this.players = players;
+    }
+
+    public void dealCards() {
+        List<Card> playableDeck = deck.init();
+        Collections.shuffle(playableDeck);
+
+        ListIterator<Card> cardListIterator = playableDeck.listIterator();
+        ListIterator<Player> playerListIterator = players.listIterator();
+
+        while (playerListIterator.hasNext()) { //while there is next player
+            Player currentPlayer = playerListIterator.next();
+            currentPlayer.getHand().getCards().add(cardListIterator.next()); //give that player a card
+            cardListIterator.remove(); //remove card from deck
+
+            if (playableDeck.size() == 0) return;
+
+            if (!playerListIterator.hasNext()) { //if card was dealt to the last player
+                playerListIterator = players.listIterator(); } //reset iterator
+        }
+    }
+}
