e581acff9159985f18824bd17633c5680387eb18
Seweryn91 <s.talaj@gmail.com>
2020-01-09 21:14:58 +0100

    Add fields, implement constructor, getters and setters

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,4 +1,39 @@
 package model;
 
 public class Player {
+
+    private Hand hand;
+    private String name;
+    private boolean isActive;
+
+
+    public Player(String name) {
+        this.name = name;
+        this.hand = new Hand();
+        this.isActive = true;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
 }
