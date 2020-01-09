c2c21937ee7ef7d96eb8b6fce9e0884c66d4299d
Seweryn91 <s.talaj@gmail.com>
2020-01-09 21:06:43 +0100

    Implement constructor and methods

--- src/main/java/model/Deck.java
+++ src/main/java/model/Deck.java
@@ -1,4 +1,40 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Deck {
+
+    private int lowestCardValue;
+
+    public Deck(int lowestCardValue) {
+        this.lowestCardValue = lowestCardValue;
+    }
+
+    public int getLowestCardValue() {
+        return lowestCardValue;
+    }
+
+    public void setLowestCardValue(int lowestCardValue) {
+        this.lowestCardValue = lowestCardValue;
+    }
+
+    public List<Card> init() {
+        List<Card> deck = new ArrayList<>();
+
+        for (int i = lowestCardValue; i < 15; i++) {
+            deck.add(new Card(Suit.SPADES, i));
+        }
+        for (int i = lowestCardValue; i < 15; i++) {
+            deck.add(new Card(Suit.CLUBS, i));
+        }
+        for (int i = lowestCardValue; i < 15; i++) {
+            deck.add(new Card(Suit.DIAMONDS, i));
+        }
+        for (int i = lowestCardValue; i < 15; i++) {
+            deck.add(new Card(Suit.HEARTS, i));
+        }
+        deck.add(new Card(Suit.JOKER, 15));
+        return deck;
+    }
 }
