8ab1857b8c7b71b35a8f635e837e2192a48a53b3
Seweryn91 <s.talaj@gmail.com>
2020-01-09 21:36:07 +0100

    Implement methods

--- src/main/java/controller/CardComparatorImpl.java
+++ src/main/java/controller/CardComparatorImpl.java
@@ -1,4 +1,23 @@
 package controller;
 
-public class CardComparatorImpl {
+import model.Card;
+
+public class CardComparatorImpl implements CardComparator {
+
+    @Override
+    public boolean compareSuit(Card card1, Card card2) {
+        return card1.getSuit() == card2.getSuit();
+    }
+
+    @Override
+    public boolean compareValue(Card card1, Card card2) {
+        return card1.getValue() == card2.getValue();
+    }
+
+    @Override
+    public boolean areCardsDiscardable(Card card1, Card card2) {
+        if (!compareSuit(card1, card2)) {
+            return compareValue(card1, card2);
+        } else return true;
+    }
 }
