7ac00926596d0fafc07b079973e5c8dc2f150c17
Seweryn91 <s.talaj@gmail.com>
2020-01-09 21:10:36 +0100

    Add field hand, implement getter and setter

--- src/main/java/model/Hand.java
+++ src/main/java/model/Hand.java
@@ -1,4 +1,17 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hand {
+
+    private List<Card> hand = new ArrayList<>();
+
+    public List<Card> getCards() {
+        return hand;
+    }
+
+    public void setCards(List<Card> hand) {
+        this.hand = hand;
+    }
 }
